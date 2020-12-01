a8386fd02870dfe229a19dc96c66ada899acfb0a
John Doe <jdoe12345JohnDoejdoe12345@outlook.com>
2020-12-01 07:43:33 +0100

    Day 1 p1/p2 solved

--- Days/Days.cs
+++ Days/Days.cs
@@ -20,9 +20,45 @@ public static partial class Days
 
   public static string Day1()
   {
-    var input = File.ReadAllLines(Path.Combine(InputBasePath, "Day1.txt"));
+    var input = File.ReadAllLines(Path.Combine(InputBasePath, "Day1.txt")).Select(int.Parse);
+    var inputStack = new Stack<int>(input);
 
-    return OutputResult(input[0], input[1]);
+    var part1 = string.Empty; //For part 1, we need to find a pair of numbers that together sum to 2020.
+
+    while (part1 == string.Empty)
+    {
+      var current = inputStack.Pop();
+
+      if (current.CanSumTo(2020, inputStack, out var candidate))
+      {
+        part1 = (current * candidate).ToString();
+      }
+    }
+
+    var part2 = string.Empty; //For part 2, we need to find three numbers that together sum to 2020.
+
+    inputStack = new Stack<int>(input);
+
+    while (part2 == string.Empty)
+    {
+      var current = inputStack.Pop();
+
+      foreach (var next in inputStack.ToArray())
+      {
+        if ((current + next).CanSumTo(2020, new Stack<int>(inputStack.Except(new[] { next })), out var candidate))
+        {
+          part2 = (current * next * candidate).ToString();
+        }
+      }
+    }
+
+    return OutputResult(part1, part2);
+  }
+
+  private static bool CanSumTo(this int source, int target, Stack<int> numbers, out int candidate)
+  {
+    candidate = numbers.FirstOrDefault(x => x == (target - source));
+    return candidate != 0;
   }
 
   #endregion
